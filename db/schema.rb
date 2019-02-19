4c499d36aadf7d1e26f67869fc8a503562892d69
shimishimizu <h200l700@gmail.com>
2019-02-19 11:28:37 +0000

    Add inquiry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_082356) do
+ActiveRecord::Schema.define(version: 2019_02_19_095358) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 2019_02_17_082356) do
   create_table "information_images", force: :cascade do |t|
     t.integer "information_id"
     t.string "information_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inquiries", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -74,6 +79,8 @@ ActiveRecord::Schema.define(version: 2019_02_17_082356) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
