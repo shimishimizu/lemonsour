2826284344f77a0f3b7e5e74ce64207292c9868f
shimishimizu <h200l700@gmail.com>
2019-02-22 11:50:30 +0000

    Add information order desc

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -2,7 +2,7 @@ class InformationsController < ApplicationController
  	before_action :authenticate_user!
 
   def index
-  	@informations = Information.page(params[:page]).per(5)
+  	@informations = Information.page(params[:page]).order('updated_at desc').per(5)
   end
 
   def show
