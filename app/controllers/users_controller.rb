4a47ebf0b7d60454062552ed99b73eecf499946b
shimishimizu <h200l700@gmail.com>
2019-02-22 12:28:57 +0000

    Add correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
    before_action :authenticate_user!
+   before_action :correct_user, only: [:edit, :update]
 
   def index
     @product = Product.find(params[:product_id])
@@ -28,5 +29,12 @@ class UsersController < ApplicationController
   private
   def user_params
   	params.require(:user).permit(:name, :profile_image, :email, :introduction)
+  end
+
+  def correct_user
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to root_path
+    end
   end
 end
